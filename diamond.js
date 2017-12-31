d12d3450506dc1be859d6536c154a80a9a94421c
Paul Wilkins <paul.m.wilkins@gmail.com>
2018-01-01 01:18:30 +1300

    Using charLine() and prevChar() functions

--- diamond.js
+++ diamond.js
@@ -1,6 +1,21 @@
-"use strict";
-module.exports = {
-    create: function (char) {
+module.exports = (function iife() {
+    "use strict";
+    function charLine(char) {
+        var codeForA = String("A").charCodeAt(0);
+        var n = char.charCodeAt(0) - codeForA;
+        if (n < 1) {
+            return "A";
+        }
+        return char + " ".repeat(2 * n - 1) + char;
+    }
+    function prevChar(char) {
+        var charCode = char.charCodeAt(0);
+        if (char === "A") {
+            return;
+        }
+        return String.fromCharCode(charCode - 1);
+    }
+    function create(char) {
         var diamond = [];
         if (!char) {
             return diamond;
@@ -10,20 +25,22 @@ module.exports = {
             return diamond;
         }
         if (char === "B") {
-            diamond = [
-                " A ",
-                char + " " + char,
-                " A "
-            ];
+            diamond.push(charLine(char));
+            char = prevChar(char);
+            diamond.unshift(" " + charLine(char) + " ");
+            diamond.push(" " + charLine(char) + " ");
             return diamond;
         }
-        diamond = [
-            "  A  ",
-            " B B ",
-            char + "   " + char,
-            " B B ",
-            "  A  "
-        ];
+        diamond.push(charLine(char));
+        char = prevChar(char);
+        diamond.unshift(" " + charLine(char) + " ");
+        diamond.push(" " + charLine(char) + " ");
+        char = prevChar(char);
+        diamond.unshift("  " + charLine(char) + "  ");
+        diamond.push("  " + charLine(char) + "  ");
         return diamond;
     }
-};
+    return {
+        create
+    };
+}());
